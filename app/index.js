c99af70ecb381132a1607b668a60ecb34bedcfef
kievking <jinlongren100@gmail.com>
2021-04-29 09:32:29 +0200

    Set up linting

---
+++ app/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express();
+app.use(express.static('public'));
+app.post('/add', (req, res) => {
+  res.json({ status: 'success' });
+});
+
+app.post('/get', (req, res) => {
+  res.json({ status: 'success' });
+});
+
+app.post('/delete', (req, res) => {
+  res.json({ status: 'success' });
+});
+
+app.post('/showAll', (req, res) => {
+  res.json({ status: 'success' });
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
+});
