5221b1850f6c8007b461954a8f2a2c0683c548fc
kievking <jinlongren100@gmail.com>
2021-05-05 15:19:43 +0200

    Tried to fix recursive invoking issue

--- app/interfaces/iChord.js
+++ app/interfaces/iChord.js
@@ -17,35 +17,33 @@ async function lookup(call, callback) {
       // Means it's at the beginning of the linked list but the hash value is still smaller
       return callback(null, { successor: this.address });
     }
-    if (sha1(this.predecessor) > fromStringToDecimal(id)) {
-      return callback(null, { successor: this.address, predecessor: this.predecessor });
+    if (sha1(this.predecessor) >= fromStringToDecimal(id)) {
+      try {
+        const response = await this.execChordRpc(this.predecessor, 'lookup', { name: id });
+        return callback(null, response);
+      } catch (e) {
+        console.log(`Error in forward routing is ${JSON.stringify(e)}`);
+      }
     }
-    try {
-      const response = await this.execChordRpc(this.predecessor, 'lookup', { name: id });
-      return callback(null, response);
-    } catch (e) {
-      console.log(`Error in forward routing is ${JSON.stringify(e)}`);
-    }
-  } else if (fromStringToDecimal(thisId) < fromStringToDecimal(id)) {
+    return callback(null, { successor: this.address, predecessor: this.predecessor });
+  }
+  if (fromStringToDecimal(thisId) < fromStringToDecimal(id)) {
     if (this.successor === TAIL) {
       // Means it's at the end of the linked list but the hash value is still bigger
       return callback(null, { predecessor: this.address });
     }
-    if (sha1(this.successor) > fromStringToDecimal(id)) {
-      return callback(null, { successor: this.successor, predecessor: this.address });
+    if (sha1(this.successor) <= fromStringToDecimal(id)) {
+      try {
+        const response = await this.execChordRpc(this.successor, 'lookup', { name: id });
+        return callback(null, response);
+      } catch (e) {
+        console.log(`Error in backward routing is ${JSON.stringify(e)}`);
+      }
     }
-    try {
-      const response = await this.execChordRpc(this.successor, 'lookup', { name: id });
-      return callback(null, response);
-    } catch (e) {
-      console.log(`Error in backward routing is ${JSON.stringify(e)}`);
-    }
-  } else {
-    // If it's the same id, just send successor because successor is always preferred
-    return callback(null, { successor: this.address });
+    return callback(null, { successor: this.successor, predecessor: this.address });
   }
-
-  return callback(null, new Error());
+  // If it's the same id, just send successor because successor is always preferred
+  return callback(null, { successor: this.address });
 }
 
 async function notify(call, callback) {
